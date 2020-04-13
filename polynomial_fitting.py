95b7861f2c74c75efc068c241437a23c7eb9f3a5
TiberiuGingu <56873135+TiberiuGingu@users.noreply.github.com>
2020-04-13 17:06:48 +0300

    CMMP+fitting

---
+++ polynomial_fitting.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def polyfit(x,y):
+    A = []
+    for i in x:
+        v = [i**4, i**3, i*i, i]
+        A.append(v)
+    return A
+    
+
+def main():
+    x = [1, 2, 3]
+    y = [1, 2, 3]
+    M = polyfit(x,y)
+    print(M)
+
+if __name__=='__main__':
+    main()
