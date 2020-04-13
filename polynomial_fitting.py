a018abc725950de3e758f90cffbd56fdea94bb2e
Dinu <dinu.enescu@gmail.com>
2020-04-13 17:38:22 +0300

    Am adaugat CMMP

--- polynomial_fitting.py
+++ polynomial_fitting.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from ml_functions import TORT, UTRIS
+
+def CMMP(A, b):
+    [A, U, beta] = TORT(A)
+    m = len(A[:, 1])
+    n = len(A[1, :])
+
+    for k in range (0, n):
+        sum = 0
+    
+        for i in range (k, m+1):
+            sum = sum + U[i][k] * b[i]
+    
+        sigma = sum/beta[k]
+    
+        for i in range (k, m+1):
+            b[i] = b[i] - sigma * U[i][k]
+
+    x = UTRIS(A, b)
+    return x
+
 
 def polyfit(x,y):
     A = []
